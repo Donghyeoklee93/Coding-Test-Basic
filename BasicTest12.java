ad280316b357123951c22047fd32f8868c5e41a5
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-03-02 22:29:49 +0000

    Add files via upload

---
+++ BasicTest12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BasicTest12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+
+        System.out.printf("%d %d", number1, number2);
+
+//        System.out.println();
+//        System.out.println();
+//
+//        System.out.println(number1 +' '); //33
+//        System.out.println(' '+ number2); //34
+//        System.out.println(number1 +' '+ number2); //35
+//
+//        System.out.println();
+//        System.out.println(number1 +" ");
+//        System.out.println(" "+ number2);
+//        System.out.println(number1 +" "+ number2);
+
+
+    }
+}
+
