c4cffd647f90c03c889f4adf9a8f3e086e19bbad
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-03-02 22:30:31 +0000

    Add files via upload

---
+++ BasicTest15.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BasicTest15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.printf("%d %d %d", number, number, number);
+    }
+}
