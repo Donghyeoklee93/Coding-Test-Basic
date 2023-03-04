b78168d3e029733973911b53cd0323e72bed2ac9
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-03-04 12:12:47 +0000

    Add files via upload

---
+++ BasicTest37.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BasicTest37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long number1 = sc.nextLong();
+        long number2 = sc.nextLong();
+//        int number3 = sc.nextInt(); // 1073741824
+//        int number4 = sc.nextInt(); // 1073741824
+//        System.out.println(number3 + number4); // -2147483648
+
+        System.out.println(number1 + number2);
+
+    }
+}
+
+
